7b918e54fa500135ae5a8dceae55040cc144d3be
梁朝伟 <minghaogood@qq.com>
2020-03-03 16:02:52 +0800

    没有token自动跳转登录页

--- admin/js/http.js
+++ admin/js/http.js
@@ -3,6 +3,10 @@ $.ajaxSetup({
     // 全局添加请求头
     headers:{
         Authorization: localStorage.getItem('token')
+    },
+    // 没有token自动跳转登录页
+    error:function(){
+        location.href='./login.html'
     }
 });
 
